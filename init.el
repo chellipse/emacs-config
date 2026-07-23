7934f3ae0cfffe5a87f11b9f81822267a8dd8867
Chell L. <pumpkinbiscuit@proton.me>
2026-07-22 20:42:34 -0700

    set read-process-output-max (* 1024 1024)

--- init.el
+++ init.el
@@ -46,6 +46,8 @@
 (when (boundp 'pgtk-wait-for-event-timeout)
   (set 'pgtk-wait-for-event-timeout 0.0001))
 
+(setq read-process-output-max (* 1024 1024))
+
 (push '(menu-bar-lines . 0)   default-frame-alist)
 (push '(tool-bar-lines . 0)   default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
